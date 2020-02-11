63322e07e728ec555fd30f5d9dfd4706df79a272
PiusAremieye <ebi44ril@gmail.com>
2020-02-11 15:32:47 +0100

    added unit test to the application

--- src/test/java/com/example/todoapp/unit/TaskControllerTest.java
+++ src/test/java/com/example/todoapp/unit/TaskControllerTest.java
@@ -1,23 +1,13 @@
 package com.example.todoapp.unit;
 
-import com.example.todoapp.controllers.TasksController;
-import com.example.todoapp.models.Task;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-
-import static org.hamcrest.Matchers.containsString;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -26,79 +16,69 @@ public class TaskControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
-    Task t1 = new Task("test1", "description for test1");
-
-    private static String stringAsJson(final Object obj){
-        try {
-            return new ObjectMapper().writeValueAsString(obj);
-        } catch (JsonProcessingException e) {
-            throw new RuntimeException(e.getMessage());
-        }
+    @Test
+    public void createTodo() throws Exception {
+        this.mockMvc.perform(get("/todos/add")
+        ).andDo(
+                print()
+        ).andExpect(
+                status().isOk()
+        ).andExpect(
+                model().attributeExists("newTodo")
+        );
     }
-
-//    @Test
-//    public void createTask() throws Exception {
-//        this.mockMvc.perform(post("/todos").contentType(MediaType.APPLICATION_JSON).content(stringAsJson(t1))
-//        ).andDo(
-//                print()
-//        ).andExpect(
-//                status().isCreated()
-//        ).andExpect(
-//                content().string(containsString("test1"))
-//        );
-//    }
-
-//    @Test
-//    public void createTask() throws Exception {
-//    this.mockMvc.perform(get("/todos"))
-//            .andExpect(status().isOk())
-//            .andExpect(view().name("/todos/add"));
-//    }
 
     @Test
     public void viewAllTodo() throws Exception {
-        this.mockMvc.perform(get("/todos").contentType(MediaType.APPLICATION_JSON).content(stringAsJson(t1))
+        this.mockMvc.perform(get("/todos")
         ).andDo(
                 print()
         ).andExpect(
                 status().isOk()
-        );
-    }
-
-    @Test
-    public void viewATodo() throws Exception {
-        this.mockMvc.perform(get("/todos").contentType(MediaType.APPLICATION_JSON).content(stringAsJson(t1))
-        ).andDo(
-                print()
         ).andExpect(
-                status().isOk()
-        );
-    }
-
-    @Test
-    public void createTodo() throws Exception {
-        this.mockMvc.perform(get("/todos/add").contentType(MediaType.APPLICATION_JSON).content(stringAsJson(t1))
-        ).andDo(
-                print()
-        ).andExpect(
-                status().isOk()
+                model().attributeExists("todos")
         );
     }
 
     @Test
     public void viewByStatus() throws Exception {
-        this.mockMvc.perform(get("/todos/status").contentType(MediaType.APPLICATION_JSON).content(stringAsJson(t1))
+        this.mockMvc.perform(get("/todos/status")
         ).andDo(
                 print()
         ).andExpect(
                 status().isOk()
+        ).andExpect(
+                model().attributeExists("todos")
         );
     }
 
     @Test
-    public void viewTodo() throws Exception {
-        ;
-        this.mockMvc.perform(get("/todo/").contentType(MediaType.APPLICATION_JSON).content(stringAsJson(t1))
+    public void deleteTodo() throws Exception {
+        this.mockMvc.perform(get("/todo/delete/1")
+        ).andDo(
+                print()
+        ).andExpect(
+                status().is3xxRedirection()
+        ).andExpect(
+                redirectedUrl("/todos")
+        );
+    }
+
+    @Test
+    public void editTodo() throws Exception {
+        this.mockMvc.perform(get("/todo/edit/2")
+        ).andDo(
+                print()
+        ).andExpect(
+                status().isOk()
+        ).andExpect(
+                model().attributeExists("editTodo")
+        );
+    }
+
+    @Test
+    public void home() throws Exception {
+        this.mockMvc.perform(get("/")
         ).andDo(
                 print()
         ).andExpect(
